7cd65adce63760aab13203125dbe1f5493f43ac8
Jaime <jaimedapontealgaba@gmail.com>
2023-09-26 20:14:21 +0200

    Primera versión

---
+++ src/main/java/com/daponte/git/Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.daponte.git;
+
+/**
+ *
+ * @author jaime
+ */
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
